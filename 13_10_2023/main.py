192bc1ef123a775805139391274c34f9bbc7f587
UIT.20520646 <20520646@gm.uit.edu.vn>
2023-10-13 12:24:58 +0700

    sua file main.py

--- 13_10_2023/main.py
+++ 13_10_2023/main.py
@@ -1,36 +1,46 @@
 #kieu du lieu
 
-#int
-int_var = 2
-print(type(int_var))
-#float
-float_var = 0.12
-print(type(float_var))
-#string
-str_var1 = 'dfgdfgdfgdf'
-str_var2 = "fsdfgadsas"
-print(type(str_var1))
-print(type(str_var2))
-#list
-list_var = [123,321,'dfsdf']
-print(type(list_var))
-#set
-set_var = set([1,24,24,241,41,341])
-print(type(set_var))
-#dict
-dict_var = {
-    "school":{
-        "name":"uit"
-    }
-}
-print(type(dict_var))
-#bool
-bool_var = True
-print(type(bool_var))
-#tuple
-tuple_var = (list_var,3143)
-print(type(tuple_var))
+# #int
+# int_var = 2
+# print(type(int_var))
+# #float
+# float_var = 0.12
+# print(type(float_var))
+# #string
+# str_var1 = 'dfgdfgdfgdf'
+# str_var2 = "fsdfgadsas"
+# print(type(str_var1))
+# print(type(str_var2))
+# #list
+# list_var = [123,321,'dfsdf']
+# print(type(list_var))
+# #set
+# set_var = set([1,24,24,241,41,341])
+# print(type(set_var))
+# #dict
+# dict_var = {
+#     "school":{
+#         "name":"uit"
+#     }
+# }
+# print(type(dict_var))
+# #bool
+# bool_var = True
+# print(type(bool_var))
+# #tuple
+# tuple_var = (list_var,3143)
+# print(type(tuple_var))
 
+
+#cau lenh dk
+
+num = int(input("nhap vao so tu 1 den 12: "))
+if num == 2:
+    print("28 ngay")
+elif num == 4 or num == 6 or num == 9 or num == 11:
+    print("30 ngay")
+else:
+    print("31 ngay")
 
 
 
